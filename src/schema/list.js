fa803a0ea6a9917b405796094f953a12cbd080bf
shadowprompt <huixiong.cn@gmail.com>
2019-06-04 16:16:12 +0800

    [fix] 优化列表查询传参

--- src/schema/list.js
+++ src/schema/list.js
@@ -1,17 +1,24 @@
 export default `
-query{
-  data: posts(post_type: "post", post_status: "publish"){
+query($currentPage: Int, $slug: String){
+  data: posts(currentPage: $currentPage, slug: $slug, pageSize: 10, post_type: "post", post_status: "publish", order:["ID", "DESC"]){
     ID
     post_title
     post_content
     post_date
+    total
     tags{
+      term_taxonomy_id
+      term_id
       name
       slug
+      term_group
     }
     categories{
+      term_taxonomy_id
+      term_id
       name
       slug
+      term_group
     }
   }
 }
