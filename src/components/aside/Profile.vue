070080b027d5bc0d6ef590a07d4239c7998a6cc5
shadowprompt <huixiong.cn@gmail.com>
2020-04-05 11:12:57 +0800

    [fix] 更换左侧边栏显示图片

--- src/components/aside/Profile.vue
+++ src/components/aside/Profile.vue
@@ -1,26 +1,20 @@
 <template>
   <div class="brand-wrap">
     <div class="brand">
-      <a href="/" class="avatar waves-effect waves-circle waves-light">
+      <div class="avatar waves-effect waves-circle waves-light">
         <img :src="avatarUrl" />
-      </a>
+      </div>
         <h5 class="nickname">普若木特</h5>
-        <a
-          href="mailto:634206017@qq.com"
-          title="634206017@qq.com"
-          class="mail"
-        >634206017@qq.com</a
-        >
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'DZProfile',
+    name: 'Profile',
     data() {
       return {
-        avatarUrl: require('../../assets/logo.png'),
+        avatarUrl: require('../../assets/owner.jpg'),
       };
     }
   }
