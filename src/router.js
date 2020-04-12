c47a7c9af61d79ab36487ef2ab4cc6b16046df60
shadowprompt <huixiong.cn@gmail.com>
2020-04-12 16:00:40 +0800

    [feat] 加入一个layout层

--- src/router.js
+++ src/router.js
@@ -1,63 +1,71 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue';
+import Router from 'vue-router';
 
 Vue.use(Router);
 
 export function createRouter() {
   return new Router({
-    mode: "history",
+    mode: 'history',
     base: process.env.BASE_URL,
     routes: [
       {
-        path: "/",
-        name: "indexList",
-        component: () => import("./views/indexPostList.vue")
+        path: '/',
+        name: 'base',
+        component: () => import('./views/layout.vue'),
+        redirect:  'indexList',
+        children: [
+          {
+            path: '/',
+            name: 'indexList',
+            component: () => import('./views/indexPostList.vue'),
+          },
+          {
+            path: '/page/:currentPage',
+            name: 'indexListPage',
+            component: () => import('./views/indexPostList.vue'),
+          },
+          {
+            path: '/:id.html',
+            name: 'detail',
+            component: () => import('./views/postDetail.vue'),
+          },
+          {
+            path: '/category/:slug',
+            name: 'categoryList',
+            component: () => import('./views/categoryPostList.vue'),
+          },
+          {
+            path: '/category/:slug/page/:currentPage',
+            name: 'categoryListPage',
+            component: () => import('./views/categoryPostList.vue'),
+          },
+          {
+            path: '/tag/:slug',
+            name: 'tagList',
+            component: () => import('./views/tagPostList.vue'),
+          },
+          {
+            path: '/tag/:slug/page/:currentPage',
+            name: 'tagListPage',
+            component: () => import('./views/tagPostList.vue'),
+          },
+          {
+            path: '/date/:year',
+            name: 'dateListYear',
+            component: () => import('./views/datePostList.vue'),
+          },
+          {
+            path: '/date/:year/:month/',
+            name: 'dateListYearMonth',
+            component: () => import('./views/datePostList.vue'),
+          },
+          {
+            path: '/date/:year/:month/:day',
+            name: 'dateListYearMonthDay',
+            component: () => import('./views/datePostList.vue'),
+          },
+        ],
       },
-      {
-        path: "/page/:currentPage",
-        name: "indexListPage",
-        component: () => import("./views/indexPostList.vue")
-      },
-      {
-        path: "/:id.html",
-        name: "detail",
-        component: () => import("./views/postDetail.vue")
-      },
-      {
-        path: "/category/:slug",
-        name: "categoryList",
-        component: () => import("./views/categoryPostList.vue")
-      },
-      {
-        path: "/category/:slug/page/:currentPage",
-        name: "categoryListPage",
-        component: () => import("./views/categoryPostList.vue")
-      },
-      {
-        path: "/tag/:slug",
-        name: "tagList",
-        component: () => import("./views/tagPostList.vue")
-      },
-      {
-        path: "/tag/:slug/page/:currentPage",
-        name: "tagListPage",
-        component: () => import("./views/tagPostList.vue")
-      },
-      {
-        path: "/date/:year",
-        name: "dateListPage",
-        component: () => import("./views/datePostList.vue")
-      },
-      {
-        path: "/date/:year/:month/",
-        name: "dateListPage",
-        component: () => import("./views/datePostList.vue")
-      },
-      {
-        path: "/date/:year/:month/:day",
-        name: "dateListPage",
-        component: () => import("./views/datePostList.vue")
-      },
-    ]
+    ],
   });
 }
---
+++ src/views/layout.vue
@@ -0,0 +1,150 @@
+<template>
+  <div id="app">
+    <div id="loading" :class="isLoading ? 'active' : ''"></div>
+    <div class="main-con">
+      <header class="top-header" :class="{ fixed: topFixed }">
+        <div class="flex-row">
+          <span class="top-icon bars-wrap" @click="TOGGLE_COLLAPSE()">
+            <i class="fa fa-bars"></i>
+          </span>
+          <div class="flex-col header-title ellipsis">
+            {{ config.static.siteName }}
+          </div>
+          <search />
+
+          <span class="top-icon share-wrap">
+            <i class="fa fa-share-alt"></i>
+          </span>
+        </div>
+      </header>
+      <section class="main-header">
+        <header class="banner-header">
+          <div class="media-container fade-scale in">
+            <h1 class="title">
+              <a class="pointer inline-contrast" href="/">
+                {{ config.static.siteName }}
+              </a>
+            </h1>
+            <h5 class="subtitle">{{ config.static.siteDes }}</h5>
+          </div>
+        </header>
+        <category-nav></category-nav>
+      </section>
+
+      <section style="display: flex">
+        <section class="media-container body-wrapper">
+          <router-view></router-view>
+        </section>
+        <slave-list></slave-list>
+      </section>
+
+      <jumper />
+      <statement />
+    </div>
+    <div class="left-con">
+      <aside-menu :show="!isCollapsed"></aside-menu>
+      <d-z-mask :visible="!isCollapsed"></d-z-mask>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import { mapState, mapMutations, mapActions } from 'vuex';
+  import DZMask from '../components/common/Mask';
+  import Jumper from '../components/common/Jumper';
+  import Statement from '../components/Statement';
+  import CategoryNav from '../components/CategoryNav';
+  import asideMenu from '../components/aside/Menu';
+  import SlaveList from '../components/slave/SlaveList';
+  import Search from '../components/Search';
+  import { config } from '../config';
+
+  export default {
+    name: 'Layout',
+    components: {
+      SlaveList,
+      Statement,
+      Jumper,
+      DZMask,
+      CategoryNav,
+      asideMenu,
+      Search,
+    },
+    asyncData({ store, route }) {
+      // 触发 action 后，会返回 Promise
+      return Promise.all([store.dispatch('_getAllCategories'), store.dispatch('_getArchiveList')]);
+    },
+    data() {
+      return {
+        config,
+        body: null,
+        goto: null,
+        prevTime: null,
+        topFixed: false,
+      };
+    },
+    provide() {
+      return config;
+    },
+    computed: {
+      ...mapState(['isLoading', 'categories', 'isCollapsed']),
+    },
+    async created() {
+      // 将this传入并设置axios拦截器
+      this.$http.setConfig(this);
+      this.$store.commit('GEN_BG_COLORS');
+      this.scrollFn = this.createDebounce(100, this.cal);
+    },
+    mounted() {
+      this.body = document.querySelector('body');
+      this.goto = document.querySelector('#goto');
+      window.document.addEventListener('scroll', this.scrollFn);
+      window.vm = this;
+    },
+    beforeDestroy() {
+      window.document.removeEventListener('scroll', this.scrollFn);
+    },
+    methods: {
+      ...mapMutations(['TOGGLE_LOADING', 'TOGGLE_COLLAPSE']),
+      ...mapActions(['_getCategories']),
+      cal() {
+        if (!this.goto) {
+          return;
+        }
+        this.topFixed = window.scrollY > 72;
+        if (window.scrollY > 210) {
+          this.goto.classList.add('show');
+        } else {
+          this.goto.classList.remove('show');
+        }
+      },
+      createDebounce(ms, fn) {
+        let prevTime;
+        return () => {
+          if (prevTime) {
+            if (Date.now() - prevTime >= ms) {
+              fn();
+              prevTime = null;
+            }
+          } else {
+            prevTime = Date.now();
+          }
+        };
+      },
+    },
+    watch: {
+      $route() {
+        // 页面跳转就开启loading效果
+        this.TOGGLE_LOADING(true);
+      },
+      isCollapsed(val) {
+        if (val) {
+          this.body.classList.remove('lock');
+        } else {
+          this.body.classList.add('lock');
+        }
+      },
+    },
+  };
+</script>
