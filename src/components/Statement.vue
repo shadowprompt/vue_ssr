02ae44c3271e98d320b1467f978d90e45b4484e2
shadowprompt <huixiong.cn@gmail.com>
2021-09-13 09:41:48 +0800

    [fix]优化代码

--- src/components/Statement.vue
+++ src/components/Statement.vue
@@ -1,8 +1,9 @@
 <template>
   <footer class="statements">
     <div class="left-state">
-      <template v-for="item in leftStatements" :key="item.href">
+      <template v-for="item in leftStatements">
         <a
+          :key="item.href"
           v-if="item.href"
           :href="item.href"
           :target="item.target || '_blank'"
@@ -14,8 +15,9 @@
       </template>
     </div>
     <div class="right-state">
-      <template v-for="item in rightStatements" :key="item.href">
+      <template v-for="item in rightStatements">
         <a
+          :key="item.href"
           v-if="item.href"
           :href="item.href"
           :target="item.target || '_blank'"
@@ -46,7 +48,7 @@ export default {
           position: 'left',
         },
         {
-          title: '鄂ICP备2020023502号',
+          title: '鄂ICP备2020023502号-3',
           href: 'https://beian.miit.gov.cn/',
           position: 'left',
         },
