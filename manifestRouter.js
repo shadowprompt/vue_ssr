e74f57a875d5456c5a739ab7565605e9556bdf2e
shadowprompt <huixiong.cn@gmail.com>
2023-03-08 00:05:13 +0800

    [fix]改用动态manifest

--- manifestRouter.js
+++ manifestRouter.js
@@ -1,46 +1,64 @@
 const router = require('express').Router();
+// const locale = require("locale");
 router.get('/', (req, res) => {
+  // const locales = new locale.Locales(req.headers["accept-language"])
   res.send({
-    name: 'Daozhao Lab',
-    short_name: 'DaozhaoLab',
-    start_url: '/?utm_source=pwa_homescreen',
-    display: 'standalone',
-    background_color: '#ffffff',
-    description: '道招网新技术研发、测试实验室！',
-    orientation: 'portrait',
-    theme_color: '#5eace0',
-    icons: [
+    name: 'Daozhao',
+    short_name: 'Daozhao',
+    "start_url": "/?utm_source=pwa_homescreen",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "description": "道招网 关注互联网|聚焦Web",
+    "orientation": "portrait",
+    "theme_color": "#df3473",
+    "icons": [
       {
-        src: '/_res/mipmap-mdpi/ic_launcher.png',
-        sizes: '48x48',
-        type: 'image/png',
+        "src": "/_res/icons/icon-72x72.png",
+        "sizes": "72x72",
+        "type": "image/png"
       },
       {
-        src: '/_res/mipmap-hdpi/ic_launcher.png',
-        sizes: '72x72',
-        type: 'image/png',
+        "src": "/_res/icons/icon-96x96.png",
+        "sizes": "96x96",
+        "type": "image/png"
       },
       {
-        src: '/_res/mipmap-xhdpi/ic_launcher.png',
-        sizes: '96x96',
-        type: 'image/png',
+        "src": "/_res/icons/icon-128x128.png",
+        "sizes": "128x128",
+        "type": "image/png"
       },
       {
-        src: '/_res/mipmap-xxhdpi/ic_launcher.png',
-        sizes: '144x144',
-        type: 'image/png',
+        "src": "/_res/icons/icon-144x144.png",
+        "sizes": "144x144",
+        "type": "image/png"
       },
       {
-        src: '/_res/mipmap-xxxhdpi/ic_launcher.png',
-        sizes: '192x192',
-        type: 'image/png',
+        "src": "/_res/icons/icon-152x152.png",
+        "sizes": "152x152",
+        "type": "image/png"
       },
       {
-        src: '/_res/web_hi_res_512.png',
-        sizes: '512x512',
-        type: 'image/png',
+        "src": "/_res/icons/icon-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png"
       },
+      {
+        "src": "/_res/icons/icon-384x384.png",
+        "sizes": "384x384",
+        "type": "image/png"
+      },
+      {
+        "src": "/_res/icons/icon-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
     ],
+    "serviceworker": {
+      "src": "/service-worker.js",
+      "scope": "/",
+      "update_via_cache": "none"
+    },
+    "gcm_sender_id": "197845115065"
   });
 });
 
