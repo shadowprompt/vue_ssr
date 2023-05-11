d8375ac9ba08f288adc31d20b97c0d214e6f4b7b
shadowprompt <huixiong.cn@gmail.com>
2023-05-11 09:05:42 +0800

    [fix]微调消息通知

--- src/components/common/Subscribe.vue
+++ src/components/common/Subscribe.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="requestNotify-area" :style="{display: this.displayNotify}">
-    <div class="close" @click="this.handleCancelNotifyClick"></div>
-    <div class="title">订阅</div>
-    <div class="content">道招网关注互联网，聚焦Web，开启订阅能接收最新。</div>
+    <div class="title">消息推送</div>
+    <div class="content">道招网关注互联网，聚焦Web，是否允许文章更新后推送通知消息。</div>
     <div class="action_area">
-      <div class="action confirm" @click="this.handleNotifyClick">订阅</div>
+      <div class="action confirm" @click="this.handleNotifyClick">允许</div>
       <div class="action cancel" @click="this.handleCancelNotifyClick">不用了</div>
     </div>
   </div>
