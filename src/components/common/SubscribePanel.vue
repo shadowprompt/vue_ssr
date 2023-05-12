b336687a2df13af97e6368e51921001cfa3972cc
shadowprompt <huixiong.cn@gmail.com>
2023-05-12 13:53:53 +0800

    [fix]加入订阅铃铛

--- src/components/common/SubscribePanel.vue
+++ src/components/common/SubscribePanel.vue
@@ -3,14 +3,20 @@
     <h5 class="title">订阅通知</h5>
     <div class="content">道招网关注互联网，分享IT资讯，前沿科技、编程技术，订阅后可及时收到更新通知。</div>
     <div class="sub_area">
-      <div :class="'sub_btn ' + subscribeStatus" @click="this.handleNotifyClick">{{subscribeStatusText}}</div>
+      <div :class="'sub_btn ' + subscribeStatus" @click="this.handleNotifyClick">
+        <Bell width="20px" height="20px"/>
+        <span class="sub_text">{{subscribeStatusText}}</span>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+import Bell from './Bell.vue';
+
 export default {
   name: 'SubscribePanel',
+  components: {Bell},
   data() {
     return {
       isSubscribed: void 0,
