e1a5649c9057c306c1ac2208de1562c6126c0433
shadowprompt <huixiong.cn@gmail.com>
2023-05-23 13:22:10 +0800

    启用PWA_HMS

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
     <router-view></router-view>
-<!--    <PWA_HMS />-->
+    <PWA_HMS />
     <Subscribe />
   </div>
 </template>
 
 <script>
-// import PWA_HMS from './PWA_HMS.vue';
+import PWA_HMS from './PWA_HMS.vue';
 import Subscribe from './components/common/Subscribe.vue';
 export default {
   name: 'App',
   components: {
-    // PWA_HMS,
+    PWA_HMS,
     Subscribe,
   }
 };
